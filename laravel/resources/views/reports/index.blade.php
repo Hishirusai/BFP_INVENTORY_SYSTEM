32082a200103cec3d9a8c8110c57841643659ab4
jelord1522 <220806@adzu.edu.ph>
2025-11-24 01:02:19 +0800

    Refactor reports view to remove unused 'From Station' column and adjust layout for better readability

--- laravel/resources/views/reports/index.blade.php
+++ laravel/resources/views/reports/index.blade.php
@@ -233,17 +233,14 @@
                             <th class="px-3 py-1.5 text-left text-[10px] font-bold uppercase tracking-wider">
                                 <i class="fas fa-tag mr-1.5 text-[10px]"></i>Type
                             </th>
-                               <th class="px-3 py-1.5 text-left text-[10px] font-bold uppercase tracking-wider">
-                                   <i class="fas fa-train mr-1.5 text-[10px]"></i>From Station
-                               </th>
-                            
+                              
                             
                             
                             <th class="px-1 py-1.5 text-center text-[10px] font-bold uppercase tracking-wider w-16">
                                 <i class="fas fa-history mr-1 text-[10px]"></i>Prev
                             </th>
 
-                            <th class="px-1 py-2.5 text-center text-[10px] font-bold uppercase tracking-wider w-26">
+                            <th class="px-1 py-2.5 text-center text-[10px] font-bold uppercase tracking-wider w-16">
                                 <i class="fas fa-sort-numeric-up mr-1 text-[10px]"></i>Qty Chg
                             </th>
 
@@ -279,9 +276,7 @@
                                     <i class="fas fa-circle text-[6px] mr-1"></i>{{ ucfirst($report->type) }}
                                 </span>
                                 </td>
-                                <td class="px-3 py-2 text-[9px] text-gray-700 whitespace-nowrap">
-                                    <i class="fas fa-train mr-2 text-gray-400"></i>{{ optional($report->fromStation)->name ?? '' }}
-                            </td>
+
                             
 
                             <td class="px-1 py-2 text-[9px] text-gray-700 font-medium text-center">
